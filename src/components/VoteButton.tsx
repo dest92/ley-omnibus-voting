f9852b3f454fc62691afe7543a3d1a3f49ad507f
dest92 <acebalmatias2019@gmail.com>
2024-01-23 13:52:58 -0300

    fixes

--- src/components/VoteButton.tsx
+++ src/components/VoteButton.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Button } from "@nextui-org/react";
 import { NextUIProvider } from "@nextui-org/react";
-import updateColumn from "../database/updateColumn";
+import updateColumn, { updateContraColumn } from "../database/updateColumn";
 
 interface BtnProps {
   type?: "default" | "primary" | "success" | "warning" | "danger" | "secondary";
@@ -30,25 +30,54 @@ export default function VoteButton({
     }
   }, []);
 
-  const handleVote = async () => {
+  const handleVoteTrue = async () => {
     await updateColumn();
+    setHasVoted(true);
+  };
+  const handleVoteFalse = async () => {
+    await updateContraColumn();
     setHasVoted(true);
   };
 
   if (hasVoted) {
-    return <></>;
+    return (
+      <>
+        <NextUIProvider>
+          <Button
+            color="danger"
+            variant="flat"
+            className="hover:opacity-70 text-center justify-center align-middle"
+            disabled
+          >
+            Ya votaste
+          </Button>
+        </NextUIProvider>
+      </>
+    );
   }
 
   return (
-    <NextUIProvider>
-      <Button
-        color={type}
-        variant={variant}
-        className="hover:opacity-70 text-center justify-center align-middle"
-        onClick={handleVote}
-      >
-        {text}
-      </Button>
-    </NextUIProvider>
+    <>
+      <NextUIProvider>
+        <div className="flex flex-col items-center justify-center">
+          <Button
+            color="success"
+            variant={variant}
+            className="hover:opacity-70 mb-2" // Añadido mb-2 para espacio entre los botones
+            onClick={handleVoteTrue}
+          >
+            Sí, estoy a favor
+          </Button>
+          <Button
+            color="danger"
+            variant={variant}
+            className="hover:opacity-70"
+            onClick={handleVoteFalse}
+          >
+            No, estoy en contra
+          </Button>
+        </div>
+      </NextUIProvider>
+    </>
   );
 }
